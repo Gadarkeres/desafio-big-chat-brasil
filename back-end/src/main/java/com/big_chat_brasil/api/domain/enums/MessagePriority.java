f28abe3f782490f0fd9af8884a2e28ab005a991c
Matheus Rafael <matheusrafael77@hotmail.com>
2026-06-18 21:08:13 -0300

    fix: ajusta serialização de json a partir do enum

--- back-end/src/main/java/com/big_chat_brasil/api/domain/enums/MessagePriority.java
+++ back-end/src/main/java/com/big_chat_brasil/api/domain/enums/MessagePriority.java
@@ -1,6 +1,23 @@
 package com.big_chat_brasil.api.domain.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum MessagePriority {
     NORMAL,
-    URGENT
+    URGENT;
+
+    @JsonCreator
+    public static MessagePriority fromString(String value) {
+        if (value == null || value.isBlank()) {
+            return null;
+        }
+
+        return MessagePriority.valueOf(value.trim().toUpperCase());
+    }
+
+    @JsonValue
+    public String toJson() {
+        return name().toLowerCase();
+    }
 }
